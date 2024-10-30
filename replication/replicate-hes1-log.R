96b6fac3939ebc3d1e19732531b70f5ed0a535d1
sophiaxxiao <sxxiao@emory.edu>
2024-10-29 21:54:31 -0400

    Deleted matlab files

--- replication/replicate-hes1-log.R
+++
@@ -1,9 +0,0 @@
-## Run this script to reproduce Hes1 oscillation system results in paper, for the same 2000 random seeds
-
-## Perform inference for the 2000 seeds (can be run in parallel)
-for (i in 1:2000) {
-  system(paste0("Rscript run-hes1-log.R ", i))
-}
-
-## Summarize results, make table and figures
-source("hes1-log-summarize.R")
--- rmagi/tests/testthat/test_bandmat.R
+++
@@ -1,6 +0,0 @@
-testthat::context("band matrix likelihood")
-testthat::test_that("band matrix likelihood runs without error", {
-  out <- magi:::bandTest(system.file("testdata/data_band.txt",package="magi"))
-  testthat::expect_equal(out, -655203.16255410481244, tolerance = 1e-3)
-})
-
